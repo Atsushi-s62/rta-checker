fbd74bc6d80a8534f0cfc0d4fbaef662793dbfd0
Atsushi-s62 <prllswi.ch01@gmail.com>
2026-02-01 22:21:16 +0900

    Update index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('css/postsStyle.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/postsStyle.css') }}?v={{ filemtime(public_path('css/postsStyle.css')) }}">
     <title>RTAchecker</title>
 </head>
 <body>
